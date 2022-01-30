c71431d0ded9a9d87f2baa6345507bb2cddc17e1
phantombass <tmkeaton@ngu.edu>
2022-01-29 19:18:50 -0500

    0.1.8-fixes

--- Plugins/Fancy Badges/Script.rb
+++ Plugins/Fancy Badges/Script.rb
@@ -83,6 +83,8 @@ def renderBadgeAnimation(badge_number = 0)
     $game_variables[106] = 21
   elsif badges == 1
     $game_variables[106] = 25
+  elsif badges == 2
+    $game_variables[106] = 38
   end
   height = Graphics.height
   screen = Graphics.snap_to_bitmap
