d462d31862ce6dea6c6fd95916f01ebebaf55d07
phantombass <tmkeaton@ngu.edu>
2022-03-12 13:34:08 -0500

    More fixes

--- Plugins/Pokemon Hegemony Full Script/01_Phantombass_Scripts.rb
+++ Plugins/Pokemon Hegemony Full Script/01_Phantombass_Scripts.rb
@@ -366,23 +366,58 @@ def pbStartOver(gameover=false)
     if gameover
       pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]After the unfortunate defeat, you scurry back to a Pokémon Center."))
     else
-      if $game_switches[73] == true
+      if $game_switches[73] == true && !$game_map.map_id == 144 && !$game_map.map_id == 145
         pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]After losing the Nuzlocke, you scurry back to a Pokémon Center, protecting your exhausted Pokémon from any further harm..."))
+        pbCancelVehicles
+        pbRemoveDependencies
+        $game_switches[Settings::STARTING_OVER_SWITCH] = true
+        $game_switches[73] = false
+        $CanToggle = true
+        $game_temp.player_new_map_id    = $PokemonGlobal.pokecenterMapId
+        $game_temp.player_new_x         = $PokemonGlobal.pokecenterX
+        $game_temp.player_new_y         = $PokemonGlobal.pokecenterY
+        $game_temp.player_new_direction = $PokemonGlobal.pokecenterDirection
+        $scene.transfer_player if $scene.is_a?(Scene_Map)
+        $game_map.refresh
+      elsif ($game_map.map_id == 144 && $game_switches[73] == true) || ($game_map.map_id == 145 && $game_switches[73] == true)
+        pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]You were captured and sent back to the cell after losing the Nuzlocke..."))
+        pbCancelVehicles
+        pbRemoveDependencies
+        $game_switches[Settings::STARTING_OVER_SWITCH] = true
+        $game_switches[73] = false
+        $CanToggle = true
+        $game_temp.player_new_map_id    = 144
+        $game_temp.player_new_x         = 45
+        $game_temp.player_new_y         = 6
+        $scene.transfer_player if $scene.is_a?(Scene_Map)
+        $game_map.refresh
+      elsif !$game_switches[73] && ($game_map.map_id == 144 || $game_map.map_id == 145)
+        pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]You were captured and sent back to the cell..."))
+        pbCancelVehicles
+        pbRemoveDependencies
+        $game_switches[Settings::STARTING_OVER_SWITCH] = true
+        $game_switches[73] = false
+        $CanToggle = true
+        $game_temp.player_new_map_id    = 144
+        $game_temp.player_new_x         = 45
+        $game_temp.player_new_y         = 6
+        $scene.transfer_player if $scene.is_a?(Scene_Map)
+        $game_map.refresh
       else
         pbMessage(_INTL("\\w[]\\wm\\c[8]\\l[3]You scurry back to a Pokémon Center, protecting your exhausted Pokémon from any further harm..."))
+        pbCancelVehicles
+        pbRemoveDependencies
+        $game_switches[Settings::STARTING_OVER_SWITCH] = true
+        $game_switches[73] = false
+        $CanToggle = true
+        $game_temp.player_new_map_id    = $PokemonGlobal.pokecenterMapId
+        $game_temp.player_new_x         = $PokemonGlobal.pokecenterX
+        $game_temp.player_new_y         = $PokemonGlobal.pokecenterY
+        $game_temp.player_new_direction = $PokemonGlobal.pokecenterDirection
+        $scene.transfer_player if $scene.is_a?(Scene_Map)
+        $game_map.refresh
       end
     end
-    pbCancelVehicles
-    pbRemoveDependencies
-    $game_switches[Settings::STARTING_OVER_SWITCH] = true
-    $game_switches[73] = false
-    $CanToggle = true
-    $game_temp.player_new_map_id    = $PokemonGlobal.pokecenterMapId
-    $game_temp.player_new_x         = $PokemonGlobal.pokecenterX
-    $game_temp.player_new_y         = $PokemonGlobal.pokecenterY
-    $game_temp.player_new_direction = $PokemonGlobal.pokecenterDirection
-    $scene.transfer_player if $scene.is_a?(Scene_Map)
-    $game_map.refresh
   else
     homedata = GameData::Metadata.get.home
     if homedata && !pbRgssExists?(sprintf("Data/Map%03d.rxdata",homedata[0]))
