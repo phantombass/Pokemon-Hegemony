274c9f2dddaa2792d9a8e5bd53b093b9c0c006d8
phantombass <tmkeaton@ngu.edu>
2022-09-01 10:33:08 -0400

    1.7.2

--- Data/Scripts/011_Battle/001_Battler/009_Battler_UseMove_SuccessChecks.rb
+++ Data/Scripts/011_Battle/001_Battler/009_Battler_UseMove_SuccessChecks.rb
@@ -71,7 +71,7 @@ class PokeBattle_Battler
       end
     end
     # Taunt
-    if @effects[PBEffects::Taunt]>0 && move.statusMove?
+    if @effects[PBEffects::Taunt]>0 && move.statusMove? || $gym_taunt == true
       if showMessages
         msg = _INTL("{1} can't use {2} after the taunt!",pbThis,move.name)
         (commandPhase) ? @battle.pbDisplayPaused(msg) : @battle.pbDisplay(msg)
