45efe157ee0067d7a733d9daf56489a2b5df48d9
phantombass <tmkeaton@ngu.edu>
2023-03-09 04:39:38 -0500

    3.6.12

--- Plugins/Pokemon Hegemony Full Script/01_Phantombass_Scripts.rb
+++ Plugins/Pokemon Hegemony Full Script/01_Phantombass_Scripts.rb
@@ -4,7 +4,7 @@
 module Settings
   LEVEL_CAP_SWITCH = 904
   FISHING_AUTO_HOOK     = true
-  GAME_VERSION = "3.6.11"
+  GAME_VERSION = "3.6.12"
 end
 
 Essentials::ERROR_TEXT += "[Pokémon Hegemony v#{Settings::GAME_VERSION}]\r\n"
